aec2f5437158c74eea7780f08124e7abde1e8656
Daniel <daniel.mesquita97@outlook.com>
2019-03-04 14:22:34 -0300

    Tests with jasmine

---
+++ hello/hello.js
@@ -0,0 +1,3 @@
+function sayHi(name){
+    return 'my name is ' + name + ' and I\'m learning Jasmine!';
+}
---
+++ hello/spec/hello.spec.js
@@ -0,0 +1,5 @@
+describe('Hello',function(){
+    it('say hi',function(){
+        expect(sayHi('Daniel')).toEqual('my name is aniel and I\'m learning Jasmine!')
+    })
+})
